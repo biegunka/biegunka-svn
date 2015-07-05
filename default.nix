e96a3cea0a910fb0e46a50287ee953c069c5ef1d
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-07-06 00:22:38 +0300

    oops

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }: let
   pkgs = nixpkgs.pkgs.haskell.packages.${compiler};
-  biegunka =  pkgs.callPackage ../biegunka/biegunka.nix {
+  biegunka = if pkgs ? biegunka then pkgs.biegunka else pkgs.callPackage ./biegunka.nix {
     mkDerivation = args: pkgs.mkDerivation(args // {
       buildTools = (if args ? buildTools then args.buildTools else []) ++ [ nixpkgs.pkgs.git ];
     });
